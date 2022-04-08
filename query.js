ebb1b50a54bb764f230cb3575e3187bd07036278
pieter-irsan <pieterirsan.pi@gmail.com>
2022-04-08 16:00:58 +0700

    Add error catch in query functions

--- query.js
+++ query.js
@@ -9,16 +9,24 @@ const pool = new Pool({
 
 const insertTimestamp = async () => {
     pool.query(
-        'INSERT INTO timestamps (created_at, is_updated) ' +
-        'VALUES (current_timestamp, false)'
+        `INSERT INTO timestamps (created_at, is_updated)
+        VALUES (current_timestamp, false)`, (err, result) => {
+            if (err) {
+                return console.error('Error executing query:', err.message);
+            }
+        }
     )
 }
 
 const updateTimestamp = async () => {
     pool.query(
-        'UPDATE timestamps SET ' +
-        'is_updated = true, updated_at = current_timestamp ' +
-        'WHERE is_updated = false'
+        `UPDATE timestamps SET
+        is_updated = true, updated_at = current_timestamp
+        WHERE is_updated = false`, (err, result) => {
+            if (err) {
+                return console.error('Error executing query:', err.message);
+            }
+        }
     )
 }
 
